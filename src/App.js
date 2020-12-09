a0d4e07681ce21dea2518e6514d30d44f1bbbd5a
sforaaleksander <sforaaleksander@gmail.com>
2020-12-09 21:08:48 +0100

    implement basic structure of walk element with styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavigationBar from "./components/common/navigation-menu/Navigation-bar";
 import React from "react";
+import WalkBox from "./components/common/walk/Walk-box";
 
 function App() {
   return (
@@ -9,6 +10,13 @@ function App() {
         <Route path="/">
           <h1>DOGMATE</h1>
           <NavigationBar />
+          <WalkBox
+            dog_name={"Zuzia"}
+            time={"12-02-2020 14:54"}
+            user_name={"Andrzej"}
+            walk_area={"Bieżanów"}
+            user_id={431}
+          />
         </Route>
       </div>
     </Router>
---
+++ src/components/common/walk/Walk-box.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./walk-box.css";
+
+WalkBox.propTypes = {
+  user_name: PropTypes.string,
+  user_id: PropTypes.number,
+  walk_area: PropTypes.string,
+  time: PropTypes.string.isRequired,
+  dog_name: PropTypes.string.isRequired,
+};
+
+WalkBox.prototype.default = {
+  user_name: undefined,
+  user_id: undefined,
+  walk_area: undefined,
+  time: new Date().getTime().toString(),
+};
+
+function WalkBox({ user_name, user_id, walk_area, time, dog_name }) {
+  return (
+    <>
+      <div className={"walk-wrapper"}>
+        <div className={"first-row"}>
+          <span className={"user-info"}>
+            <span className={"user-name"}>{user_name}</span>
+            <span className={"user-id"}>#{user_id}</span>
+          </span>
+          <span className={"dot"}>•</span>
+          {walk_area}
+          <span className={"dot"}>•</span>
+          <span>{time}</span>
+        </div>
+        <div className={"dog-name"}>{dog_name}</div>
+      </div>
+    </>
+  );
+}
+
+export default WalkBox;
