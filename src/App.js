dd2b87bccaec1ccf7cb7d3429e501e7818f3105f
sforaaleksander <sforaaleksander@gmail.com>
2020-12-09 19:15:58 +0100

    implement nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import NavigationBar from "./components/common/navigation-menu/Navigation-bar";
+import React from "react";
+
+
 function App() {
-  return (
-    <div>
-      <h1>DOGMATE</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Route path="/">
+
+                    <h1>DOGMATE</h1>
+                    <NavigationBar/>
+                </Route>
+
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavigationItem from "./components/common/navigation-menu/Navigation-item";
+import NavigationBar from "./components/common/navigation-menu/Navigation-bar";
 
 ReactDOM.render(
     <React.StrictMode>
