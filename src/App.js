7e2d69e6d36d17a1246015781fb6cade93fe384e
MateuszGolda <mateusz.golda1993@gmail.com>
2020-12-10 21:53:51 +0100

    set isLoggedIn default value to false, and require logging in to see other pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
 import "./index.css";
 import { LoginContext } from "./components/common/avatar-menu/LoginContext";
 import MainTemplate from "./components/common/MainTemplate";
+import Login from "./components/auth/Login";
+import Registration from "./components/auth/Registration";
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const logOut = () => {
     console.log("in logOut");
@@ -16,9 +24,22 @@ const App = () => {
   console.log("isLoggedIn = ", isLoggedIn);
   return (
     <>
-      <LoginContext.Provider value={[logOut]}>
-        {isLoggedIn && <MainTemplate />}
-        <h3>App</h3>
+      <LoginContext.Provider value={[logOut, setIsLoggedIn]}>
+        {isLoggedIn ? (
+          <MainTemplate />
+        ) : (
+          <Router>
+            <Switch>
+              <Route exact path={"/registration"}>
+                <Registration />
+              </Route>
+              <Route path={"/"}>
+                <Redirect to={"/"} />
+                <Login />
+              </Route>
+            </Switch>
+          </Router>
+        )}
       </LoginContext.Provider>
     </>
   );
