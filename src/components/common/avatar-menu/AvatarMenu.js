5b9a8e781f8de50f516a225bfdfcf23cfdd2bae1
MateuszGolda <mateusz.golda1993@gmail.com>
2020-12-10 11:23:53 +0100

    wrap Links wit Router

--- src/components/common/avatar-menu/AvatarMenu.js
+++ src/components/common/avatar-menu/AvatarMenu.js
@@ -1,23 +1,24 @@
 import { useContext } from "react";
 import "./avatarMenu.css";
-import { Link, Redirect } from "react-router-dom";
+import { Link, BrowserRouter as Router } from "react-router-dom";
 import { LoginContext } from "./LoginContext";
 
 const AvatarMenu = () => {
   const [logOut] = useContext(LoginContext);
-  console.log("++++++++" + logOut);
 
   return (
     <div className="user-menu-bar">
-      <Link to="/user-profile" className={"user-menu-link"}>
-        My profile
-      </Link>
-      <Link to="/settings" className={"user-menu-link"}>
-        Settings
-      </Link>
-      <Link to="/logout" className={"user-menu-link"} onClick={logOut}>
-        Log out
-      </Link>
+      <Router>
+        <Link to="/user-profile" className={"user-menu-link"}>
+          My profile
+        </Link>
+        <Link to="/settings" className={"user-menu-link"}>
+          Settings
+        </Link>
+        <Link to="/logout" className={"user-menu-link"} onClick={logOut}>
+          Log out
+        </Link>
+      </Router>
     </div>
   );
 };
