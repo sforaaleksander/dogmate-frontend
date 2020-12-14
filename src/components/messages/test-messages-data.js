6a62f47bf7467aaf57d1d29d3203b10817df9473
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-12-14 14:32:14 +0100

    add data mockup for messages overview

---
+++ src/components/messages/test-messages-data.js
@@ -0,0 +1,77 @@
+export const data = [
+  {
+    id: 1239,
+    name: "john",
+    img: "https://www.w3schools.com/howto/img_avatar.png",
+    dogs: [
+      {
+        name: "Isopropyl Alcohol",
+      },
+      {
+        name: "Louise",
+      },
+    ],
+    lastMessage: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    date: "2020-12-14 12:12:12",
+  },
+  {
+    id: 2451,
+    name: "peter",
+    dogs: [
+      {
+        name: "Buka",
+      },
+    ],
+    lastMessage: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    date: "2020-12-13 13:11:11",
+  },
+  {
+    id: 3312,
+    name: "susan",
+    img: "https://www.w3schools.com/w3images/avatar6.png",
+    dogs: [
+      {
+        name: "Dupa",
+      },
+    ],
+    lastMessage: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    date: "2020-12-12 12:12:12",
+  },
+  {
+    id: 4750,
+    name: "anna",
+    img: "https://www.w3schools.com/howto/img_avatar2.png",
+    dogs: [
+
+    ],
+    lastMessage: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    date: "2020-11-30 08:12:12",
+  },
+  {
+    id: 12390,
+    name: "john",
+    img: "https://www.w3schools.com/howto/img_avatar.png",
+    dogs: [
+      {
+        name: "Isopropyl Alcohol",
+      },
+      {
+        name: "Louise",
+      },
+    ],
+    lastMessage: "xD",
+    date: "2020-11-28 22:32:12",
+  },
+  {
+    id: 24512,
+    name: "peter",
+    img: "https://www.w3schools.com/w3images/avatar2.png",
+    dogs: [
+      {
+        name: "Buka",
+      },
+    ],
+    lastMessage: "Lorem ipsum dolor",
+    date: "2020-11-15 15:45:12",
+  },
+];
