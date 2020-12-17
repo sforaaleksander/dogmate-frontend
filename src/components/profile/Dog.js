b402afb5461236553e78aac306a84275a0b5d6ec
sforaaleksander <sforaaleksander@gmail.com>
2020-12-17 11:53:44 +0100

    now displays dog's sex and is neutered info

--- src/components/profile/Dog.js
+++ src/components/profile/Dog.js
@@ -3,17 +3,17 @@ import React from "react";
 import DogInfoBlock from "./Dog-info-block";
 
 function Dog({ dog }) {
-  const { id, name, gender, dateOfBirth, breed, temper, isNeutered } = dog;
+  const { name, gender, dateOfBirth, breed, temper, isNeutered } = dog;
   return (
     <div className={"dog-info"}>
       <DogInfoBlock />
       <div className={"dog-data"}>
         <div>{name}</div>
-        <div>{gender}</div>
+        <div>{gender ? "male" : "female"}</div>
         <div>{dateOfBirth}</div>
         <div>{breed.name}</div>
         <div>{temper.name}</div>
-        <div>{isNeutered}</div>
+        <div>{isNeutered ? "yes" : "no"}</div>
       </div>
     </div>
   );
