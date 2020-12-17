854449e494ba4773aec72a8d4a4ea6fa65b30c68
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-12-17 11:54:11 +0100

    create basic map component with styling

---
+++ src/components/map/Map.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function renderMap() {}
+
+function Map() {
+  return (
+    <div>
+      <div className="map-wrapper">
+        <div id="mapId"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Map;
